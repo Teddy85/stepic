c2cae1a24a9eb9c6ff6797e0be07d10d88257751
Teddy <m.fahrazeev@gmail.com>
2016-05-03 13:13:57 +0300

    11 commit

--- init.sh
+++ init.sh
@@ -4,4 +4,4 @@ sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test/
 sudo /etc/init.d/gunicorn restart
 cd /home/box/web/
-sudo gunicorn -c /home/box/web/etc/gunicorn.cong hello:app
+sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:app
