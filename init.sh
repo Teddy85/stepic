d8cfb48cbd7e1485aa93955c8bc7b08ff49fcc8d
Teddy <m.fahrazeev@gmail.com>
2016-05-03 13:31:48 +0300

    13 commit

--- init.sh
+++ init.sh
@@ -4,4 +4,4 @@ sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
 cd /home/box/web/
-sudo gunicorn -c /home/box/web/etc/hello.py hello:app
+sudo gunicorn -b 0.0.0.0:8080 hello:app
